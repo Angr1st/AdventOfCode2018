e60d41143ddbbffb5e1c687796a173c5b44d19b4
Torben Clasen <torbenclasen@gmail.com>
2018-12-03 16:19:28 +0100

    save before trying highperformance

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -4,31 +4,27 @@ open System
 open System.IO
 open System.Text
 
-type Checksum =
-    {Twice:int;
-    Thrice:int}
-
 [<Literal>]
 let inputDataPath = "./inputData.txt"
-
-let FindDuplicateLetters input =
-    let grouped =input |> Seq.groupBy (id) |> Seq.map (fun x -> (fst x, Seq.toList (snd x)|>List.length)) |> Seq.sortBy snd |> Seq.toList
-
-    let doub y=
-        if ((List.where (fun x -> snd x = y) grouped).Length > 0)
-        then 1
-        else 0
-    
-    {Twice= doub 2; Thrice= doub 3}
         
 let readInputData path =
-   File.ReadAllLines(path, Encoding.UTF8) |> Array.toList |> List.map FindDuplicateLetters
+   File.ReadAllLines(path, Encoding.UTF8) |> Array.toList
+
+let concatListTimes inputList times =
+   let mutable resultList = [] 
+   
+   for i=1 to times do
+      resultList <- inputList::resultList
+
+   resultList
 
 [<EntryPoint>]
 let main argv =
     let checksums = readInputData inputDataPath
-    let accumulate = List.fold (fun acc elem -> {Twice= acc.Twice + elem.Twice; Thrice= acc.Thrice + elem.Thrice}) {Twice=0;Thrice=0} checksums
-    let checksumResult = accumulate.Thrice * accumulate.Twice
-
-    printfn "Checksum: %i" checksumResult
+    let checksumListLength = checksums.Length
+    let duplicateValues = List.replicate checksumListLength checksums
+    //let accumulate = List.
+    //let checksumResult = accumulate.Thrice * accumulate.Twice
+    
+    //printfn "Checksum: %i" checksumResult
     0 // return an integer exit code
