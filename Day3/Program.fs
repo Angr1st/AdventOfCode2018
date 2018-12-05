6f1823b2d1f11c3fbc88501b9422c8d0db1f99e0
Torben Clasen <torbenclasen@gmail.com>
2018-12-05 23:44:07 +0100

    Defined DataTypes

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,6 +1,28 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open System.Text
+
+[<Literal>]
+let inputDataPath = "./inputData.txt"
+        
+let readInputData path =
+   File.ReadAllLines(path, Encoding.UTF8) |> Array.toList
+
+type ElfRequest =
+    {Number:int;
+    XCoord:int;
+    YCoord:int;
+    XSize:int;
+    YSize:int}
+
+type FabricPieceLocation =
+    {XCoord:int;
+    YCoord:int}
+
+type FabricPiece =
+    |NotTaken of 
 
 [<EntryPoint>]
 let main argv =
